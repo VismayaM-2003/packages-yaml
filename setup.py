8e6a6af51bd4171a5fb491d125eecd6772e55bc5
VismayaM-2003 <128399904+VismayaM-2003@users.noreply.github.com>
2023-12-05 16:44:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers =[
 
 setup(
     name='testsimpleproject' ,
-    version='0.0.1' ,
+    version='0.0.4' ,
     description='A very basic calculator' ,
     long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
     url='' ,
